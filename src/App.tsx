d1eb0ae72557b07ed0b5ac624acfc425ad3cf7fe
Dejan Mijic <dejanmijic@Dejans-MacBook-Air.local>
2026-05-20 15:39:14 +0200

    Initializing project - React + TypeScript + Vite + ESLint

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import type { JSX } from "react";
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <main>
+        <section>
+          <h1>Video capture</h1>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default App;
